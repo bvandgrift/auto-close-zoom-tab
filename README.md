c99e8c13fbbb8cdc50fcb027a75f2a573f7daced
Ben Vandgrift <ben@vandgrift.com>
2021-05-12 17:07:29 -0400

    updating regex, redistributing

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A small WebExtension that auto closes the zoom tab (https://zoom.us/) when you launch a meeting, because it doesn't do that itself for some reason.
+
+originally authored by https://github.com/andymckay
--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 function maybeClose(tabId, changeInfo, tab) {
-	if (tab.url.match(/https:\/\/([a-z]+).zoom.us\/[a-z]\/([0-9]+)\?(.*)?status=success/)) {
-		setTimeout(function() {
-			browser.tabs.remove(tab.id);
-		}, 2000);
-	}
+  if (tab.url.match(/https:\/\/([a-z0-9.]+).zoom.us\/postattendee\?(.*)/)) {
+    setTimeout(function () {
+      browser.tabs.remove(tab.id);
+    }, 2000);
+  }
 }
 
 browser.tabs.onUpdated.addListener(maybeClose);
